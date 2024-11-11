438af639be5ecbc4f99f4b34505cb53c3994e47f
Evgeny Ilyin <evgeniy.v.ilyin@gmail.com>
2024-11-12 00:01:45 +0300

    page for-graduates.himl 90%

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import * as fn from "./modules/functions.js";
 import * as sw from "./modules/swiper.js";
 import * as sl from "./modules/form-custom-select.js";
+import * as form from "./modules/form-submit.js";
+import { globalForm } from "./modules/form-submit.js";
 // import { useDynamicAdapt } from "./modules/dynamicAdapt.js";
 
 addEventListener("DOMContentLoaded", () => {
@@ -25,6 +27,10 @@ addEventListener("DOMContentLoaded", () => {
 	// useDynamicAdapt();
 	// fn.isWebp();
 	// fn.stickyHeader();
+	form.submitPrevent();
+	form.maskHandler();
+	Object.assign(window, { globalForm });
+	globalForm.validation();
 });
 
 // import "./modules/cookies.js";
