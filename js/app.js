b5937c6c8511f3a635d9c6d7e63a9448b7fb2440
Evgeny Ilyin <evgeniy.v.ilyin@gmail.com>
2024-11-19 11:38:56 +0300

    Update 2024-11-19T08:38:50.088Z

--- js/app.js
+++ js/app.js
@@ -10646,6 +10646,45 @@ addEventListener("DOMContentLoaded", () => {
 	Oe.bind("[data-fancybox]", options);
 });
 
+;// ./src/js/modules/yandex-map.js
+if (!window.mapInit) {
+	window.mapInit = () => {
+		const mainMapContainer = document.querySelector(".map-main");
+		if (!mainMapContainer) return;
+
+		let pointerIcon = data.pointerIcon,
+			zoomDefault = 14,
+			options = { suppressMapOpenBlock: true, minZoom: 5, maxZoom: 16 };
+
+		const mainMap = new ymaps.Map(
+			mainMapContainer,
+			{
+				center: [0, 0],
+				zoom: zoomDefault,
+				controls: [],
+			},
+			options
+		);
+
+		let objectManager = new ymaps.ObjectManager({});
+
+		objectManager.objects.options.set({
+			iconLayout: "default#image",
+			iconImageHref: pointerIcon,
+			iconImageSize: [45, 45],
+			balloonMaxWidth: 254,
+		});
+
+		objectManager.add(data);
+
+		mainMap.geoObjects.add(objectManager);
+		// zoomMargin нужен, если точки при getBounds оказались слишком близко к краю прямоугольника
+		mainMap.setBounds(objectManager.getBounds(), { checkZoomRange: true, zoomMargin: 10 }).then(function () {
+			if (mainMap.getZoom() > zoomDefault) mainMap.setZoom(zoomDefault);
+		});
+	};
+}
+
 ;// ./src/js/app.js
 
 
@@ -10684,6 +10723,7 @@ addEventListener("DOMContentLoaded", () => {
 });
 
 // import "./modules/cookies.js";
+
 
 
 })();
